730e369c794fc004e7b43a0746f73d26311fc94c
youngtaek_hong2 <youngtaek_hong2@tmax.co.kr>
2020-06-26 14:20:12 +0900

    [CSS] 제목 부분에 간단한 CSS 입력

--- src/Screens/InputScreen/DataListView/RecommendedNumber/index.tsx
+++ src/Screens/InputScreen/DataListView/RecommendedNumber/index.tsx
@@ -6,11 +6,12 @@ import { initBaseballNumbers, changeTypes, checkData, pickRandomNumber } from '.
 const Container = Styled.View`
     justify-content: center;
     align-items: center;
+    background-color: rgb(11, 166, 154);
 `;
 
 const Title = Styled.Text`
     font-size: 36px;
-    font-weight: bold
+    font-weight: bold;
 `;
 
 const NumberLabel = Styled.Text`
